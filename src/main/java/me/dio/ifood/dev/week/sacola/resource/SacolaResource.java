5af25800433e7e7304fd6d045d5a0bc097e4f5a9
cdlldanilo <danilocardilli@gmail.com>
2022-11-07 14:03:29 -0300

    Foram criadas as classes ItemDto e SacolaResource

---
+++ src/main/java/me/dio/ifood/dev/week/sacola/resource/SacolaResource.java
@@ -0,0 +1,31 @@
+package me.dio.ifood.dev.week.sacola.resource;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.ifood.dev.week.sacola.model.Item;
+import me.dio.ifood.dev.week.sacola.model.Sacola;
+import me.dio.ifood.dev.week.sacola.resource.dto.ItemDto;
+import me.dio.ifood.dev.week.sacola.service.SacolaService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+@RequiredArgsConstructor
+public class SacolaResource {
+    private final SacolaService sacolaService;
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+        return sacolaService.incluirItemNaSacola(itemDto);
+    }
+
+    @GetMapping("/{id}")
+    public Sacola verSacola(@PathVariable("id") Long id)  {
+        return sacolaService.verSacola(id);
+    }
+
+    @PatchMapping("/fecharSacola/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId, @RequestParam("formaPagamento") int formaPagamento) {
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
+    }
+
+}
